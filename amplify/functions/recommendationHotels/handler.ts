5c717e3ffd54bfa15be9f8012187641c202fa3a2
daisuke.murai <d.murai@ana-x.co.jp>
2024-09-07 09:16:34 +0900

    handler内の警告解除 and メッセージ送信時にたまにメッセージ内容がTextAreaに残る問題を解消

--- amplify/functions/recommendationHotels/handler.ts
+++ amplify/functions/recommendationHotels/handler.ts
@@ -4,15 +4,14 @@ import {
     BedrockAgentRuntimeClient,
     InvokeAgentCommand,
     InvokeAgentCommandInput,
-    InvokeAgentCommandOutput,
-    ResponseStream
+    InvokeAgentCommandOutput
 } from "@aws-sdk/client-bedrock-agent-runtime";
 
-
-export const handler: Schema["recommendationHotels"]["functionHandler"] = async (event) => {
+export const handler: Schema["recommendationHotels"]["functionHandler"] =
+    async (event) => {
     const {sessionId, inputText} = event.arguments
 
-    const params : InvokeAgentCommandInput = {
+    const params: InvokeAgentCommandInput = {
         agentId: "SRNGBSVDR3", // 使用するエージェントのIDを指定
         agentAliasId: "PDVOTJEIKF",
         sessionId: sessionId,
@@ -21,22 +20,19 @@ export const handler: Schema["recommendationHotels"]["functionHandler"] = async 
 
     const client = new BedrockAgentRuntimeClient({region: "ap-northeast-1"});
     const command = new InvokeAgentCommand(params);
-    const response: InvokeAgentCommandOutput = await client.send(command);
 
-    if (!response.completion) {
-        console.error("No completion stream found in the response");
-        return null; // completionが存在しない場合はnullを返す
+    let response = "";
+    const agentResponse: InvokeAgentCommandOutput = await client.send(command);
+
+    if (agentResponse.completion === undefined) {
+        throw new Error("Completion is undefined");
     }
 
-    const completion: AsyncIterable<ResponseStream> = response.completion;
-    const actualStream = completion.options.messageStream;
-    let finalResponse: string | null = null; // 最終的なレスポンスを格納
-
-    for await (const value of actualStream) {
-        const jsonString = new TextDecoder().decode(value.body);
-        const base64encoded = JSON.parse(jsonString).bytes;
-        finalResponse = Buffer.from(base64encoded, 'base64').toString();
+    for await (const chunkEvent of agentResponse.completion) {
+        const chunk = chunkEvent.chunk;
+        const decodedResponse = new TextDecoder("utf-8").decode(chunk?.bytes);
+        console.log(decodedResponse);
+        response += decodedResponse;
     }
-
-    return finalResponse;
+    return response
 }
