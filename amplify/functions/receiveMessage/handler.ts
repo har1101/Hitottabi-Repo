20d22e994f07d7d71b3e54b36f21a1d9c6cf5121
ryota <stryota310@gmail.com>
2024-10-01 00:28:25 +0900

    SES：予約申込完了メール送信 SQS：既存Queue利用に変更

--- amplify/functions/receiveMessage/handler.ts
+++ amplify/functions/receiveMessage/handler.ts
@@ -1,53 +1,44 @@
-import { SQSEvent, Context, Callback } from 'aws-lambda';
-import * as AWS from 'aws-sdk';
-import outputs from "../../../amplify_outputs.json"
+import { SQSEvent } from 'aws-lambda';
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 
-const sns = new AWS.SNS();
+// const sns = new AWS.SNS();
+const sesClient = new SESClient( "ap-northeast-1" );
+
+export type Message = {
+    subject: string;
+    body: string;
+    recipient: string;
+};
+
+const sendEmail = async (recipient) => {
+
+    const command = new SendEmailCommand({
+        Source: "no-reply@hitottabi.com",
+        Destination: {
+            ToAddresses: [recipient],
+        },
+        Message: {
+            Body: {
+                Text: { Data: "この度は、「ひとったび」をご利用いただきありがとうございます。\nご予約のお申し込みを受理いたしました。\n現時点では予約は完了していません。予約完了次第、再度予約完了メールにてお知らせいたします。" }
+            },
+            Subject: { Data: "予約申込が完了しました" }
+        }
+    });
+
+    try {
+        const result = await sesClient.send(command);
+        console.log(`Email sent to ${recipient}: ${result.MessageId}`);
+    } catch (error) {
+        console.error(`Error sending email to ${recipient}: ${error}`);
+        throw new Error(`Failed to send email to ${recipient}`);
+    }
+}
 
 export const handler = async (event: SQSEvent) => {
-    const snsTopicArn = outputs.custom.sns.topicArn;
-
     for (const record of event.Records) {
-
-        // メッセージからメールアドレスを取得
         const emailAddress = record.body;
 
-        // const param = {
-        //     Protocol: "email",
-        //     TopicArn: snsTopicArn,
-        //     Endpoint: emailAddress,
-        //     Attributes: {
-        //         FilterPolicy: JSON.stringify({
-        //             targetEmail: [emailAddress]
-        //         })
-        //     }
-        // }
-        // await sns.subscribe(param, function (err, data) {
-        //     if (err) {
-        //         console.log(err, err.stack); // エラー処理
-        //     } else {
-        //         console.log(data); // サブスクリプションが成功した場合
-        //     }
-        // }).promise();
-
-
-        try {
-            // SNSを使って指定のメールアドレスにメッセージを送信
-            await sns.publish({
-                Message: 'この度は、「ひとったび」をご利用いただきありがとうございます。\nご予約のお申し込みを受理いたしました。\n現時点では予約は完了していません。予約完了次第、再度予約完了メールにてお知らせいたします。',
-                Subject: '予約申込が完了しました',
-                TopicArn: snsTopicArn,  // SNSトピックにメッセージを送信
-                MessageAttributes: {
-                    'targetEmail': {
-                        DataType: 'String',
-                        StringValue: emailAddress,
-                    }
-                },
-            }).promise();
-            console.log(`Email sent to ${emailAddress}`);
-        }
-        catch (error) {
-            console.log(`publish error ${error}`)
-        }
+        // send the message via email
+        await sendEmail(emailAddress);
     }
 };
