a0abe23bf01d26fcfcfbd898eeb6a8f92415b385
ryota <stryota310@gmail.com>
2024-10-03 17:36:01 +0900

    ビルドエラー対応 'Grid' is declared but its value is never read

--- src/components/element/ActivityElement.tsx
+++ src/components/element/ActivityElement.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Button, FormControl, Grid, Typography, Stack } from "@mui/material";
+import { Box, Button, FormControl, Typography, Stack } from "@mui/material";
 import { Plan } from "../section/MainContent.tsx";
 
 export interface Activity {
